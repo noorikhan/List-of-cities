960a9051e53c12d0c279b4e36a015e1ba957c3ff
Noori Fatima <noorifatima@Akthars-MacBook-Air.local>
2022-04-21 13:54:54 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Home />
-      <Navbar />
-      <EditDetails />
-      <AddCity />
-      <AddCountry />
-      <Table /> */}
       <AllRoutes />
     </div>
   );
